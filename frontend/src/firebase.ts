c4101ace30ac8168e40194e8793dafb8e3173246
Cedric <jjy060503jjy@gmail.com>
2026-04-28 19:26:34 +0900

    RealTimeDatabase

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -6,13 +6,15 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyDYI-iCYs9iMc2CLgWbhPsbbIAvjaAARFE",
   authDomain: "space-of-us.firebaseapp.com",
-  databaseURL: "https://space-of-us-default-rtdb.firebaseio.com",
+  databaseURL: "https://space-of-us-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "space-of-us",
   storageBucket: "space-of-us.firebasestorage.app",
   messagingSenderId: "555986533527",
   appId: "1:555986533527:web:a94d639bf16425f6af3dd2",
   measurementId: "G-11YGRFKJ1H"
 };
+
+console.log("Firebase 초기화 중... DB URL:", firebaseConfig.databaseURL);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
