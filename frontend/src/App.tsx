c1724a319b6264f57663f1e451248b130ecaac62
Cedric <jjy060503jjy@gmail.com>
2026-04-28 20:54:32 +0900

    Fix: remove unused variable to fix Vercel build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { 
   onAuthStateChanged, 
   signInWithEmailAndPassword, 
-  createUserWithEmailAndPassword, 
   signOut, 
   updateProfile,
   GoogleAuthProvider, 
